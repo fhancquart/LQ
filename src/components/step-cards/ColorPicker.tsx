ebaf015b49a70d583135cf21a162aebc653159e5
fhancquart <florent.hancquart@sarbacane.com>
2021-05-14 14:51:18 +0200

    fix

--- src/components/step-cards/ColorPicker.tsx
+++ src/components/step-cards/ColorPicker.tsx
@@ -14,12 +14,14 @@ export const ColorPicker: React.FC<ColorPickerProps> = (props) => {
         borderColor: shadeColor(props.settings["color" + props.index] || "#f0f0f0",-10)
     };   
 
+    console.log(props.settings["color" + props.index])
+
     return(
         <div className="ssButton" style={colorPicker}>
             <input 
                 type="text" 
                 name={`familyInput${props.index}`} 
-                style={props.settings["color" + props.index] ? { color: 'white' } : {color: "#777777"}} 
+                style={props.settings["color" + props.index] ? { color: 'white', fontWeight: "bolder" } : {color: "#999999", fontWeight: "bolder"}} 
                 placeholder={`Famille ${props.index + 1}`} 
                 onChange={e => props.handleChange(e) } 
                 value={props.settings["familyInput" + props.index]} 
